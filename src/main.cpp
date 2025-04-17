03a6fc3bf957161fc83ec87a02de25ec2e4fa6d9
frenchfrysays <lucas.e.fry@outlook.com>
2025-04-16 17:11:47 -0700

    finish main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,3 +10,28 @@ class $modify(MyMenuLayer, MenuLayer) {
     }
 
     log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+
+    auto myButton = CCMenuItemSpriteExtra::create(
+      CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
+      this,
+      menu_selector(MyMenuLayer::onMyButton)
+    );
+
+    auto menu = this->getChildByID("bottom-menu");
+    menu->addChild(myButton);
+
+    myButton->setID("my-button"_spr);
+
+    menu->updateLayout();
+
+    return true;
+  }
+
+  void onMyButton(CCObject*) {
+    FLAlertLayer::create(
+      "Go to the source code",
+      "https://github.com/lucas-games/geode-mod",
+      "OK"
+    )->show();
+  }
+};
